5b7fa9b5a1cfb68c7d93bae2e7236e6b9988d491
Mayuran <mayuranswee23@gmail.com>
2022-03-11 23:58:46 -0500

    finished inquirer prompts and created filesystem to write HTML and copy styles

--- index.js
+++ index.js
@@ -1,25 +1,161 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager')
+const Intern = require('./lib/Intern')
+const Engineer = require('./lib/Engineer')
+// const generatePage = require('./src');
 
-const questions = () => {
+const {writeHTML, copyStyle} = require('./src/generatePage');
+const template = require('./src/page-template');
+
+const team = [];
+
+const managerPrompts = () =>{
+
+  
+
     return inquirer.prompt([
         {
-            type: 'text',
-            message: 'What is the name of the Employee?',
-            name: 'Name'
+            type: 'input', 
+            name: 'name', 
+            message: 'What is your name?',
+            validate: nameValue => {
+                if (nameValue){
+                    return true;
+                } else {
+                    console.log('Please enter your name')
+                    return false;
+                }
+            }
         },
         {
             type: 'input', 
-            message: 'What is the ID of the Employee?',
-            name: 'ID'
+            name: 'id', 
+            message: 'What is the ID of the Manager?'
+        }, 
+        {
+            type: 'input', 
+            name: 'email', 
+            message: 'What is the email address of the Manager?'
+        }, 
+        {
+            type: 'input', 
+            name: 'officeNumber', 
+            message: 'What is the office number for the Manager?'
         }
-    .then(({name})=> {
-        this.player = new Player (name)
-    })
-   
     ])
-    
+     .then(response => {
+         const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
+         team.push(manager); 
+         
+     })
 }
 
-questions()
+
+const employeePrompts = () => {
+    return inquirer.prompt([
+        {
+        type: 'list', 
+        name: 'employee', 
+        message: 'Which employee would you like to create a profile for next?', 
+        choices: ['Intern', 'Engineer', 'Exit']
+    }
+]).then(response => {
+
+    if (response.employee === 'Intern'){
+        internPrompts();
+    } else if(response.employee === "Engineer"){ 
+        engineerPrompts();
+    } else if(response.employee ==="Exit"){
+        generatePage();
+    }
+})
+}
+
+const internPrompts =()=>{
+
+   
+    return inquirer.prompt([
+        {
+            type: 'input', 
+            name: 'name', 
+            message: 'What is your name?',
+            validate: nameValue => {
+                if (nameValue){
+                    return true;
+                } else {
+                    console.log('Please enter your name')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input', 
+            name: 'id', 
+            message: 'What is the ID of the Intern?'
+        }, 
+        {
+            type: 'input', 
+            name: 'email', 
+            message: 'What is the email address of the Intern?'
+        }, 
+        {
+            type: 'input', 
+            name: 'school', 
+            message: 'What school does the Intern attend?'
+        }
+    ]).then(response=>{
+        const intern = new Intern(response.name, response.id, response.email, response.school); 
+        team.push(intern)
+        employeePrompts();
+       
+    })
+}
+
+const engineerPrompts = () => {
+
+    return inquirer.prompt([
+        {
+            type: 'input', 
+            name: 'name', 
+            message: 'What is your name?',
+            validate: nameValue => {
+                if (nameValue){
+                    return true;
+                } else {
+                    console.log('Please enter your name')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input', 
+            name: 'id', 
+            message: 'What is the ID of the Engineer?'
+        }, 
+        {
+            type: 'input', 
+            name: 'email', 
+            message: 'What is the email address of the Engineer?'
+        }, 
+        {
+            type: 'input', 
+            name: 'github', 
+            message: 'What is your Github username?'
+        }
+    ]).then(response=>{
+        
+
+        const engineer = new Engineer (response.name, response.id, response.email, response.github);
+        team.push(engineer);
+        // // console.log(data.enginner);
+        employeePrompts();
+    })
+}
+
+const generatePage = () => {
+    fs.writeFileSync('./dist/index.html', template(team))
+}
+
+
+managerPrompts()
